002d42ec32638b3f3f3ee897fd20fd3dcf8936c3
TurosIgor <valyp99@gmail.com>
2024-10-07 16:34:17 +0200

    Creating separate file for the teams useReducer

---
+++ taboo/src/contexts/TeamContext/TeamContext.jsx
@@ -0,0 +1,54 @@
+export const teamReducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_PLAYER":
+            return state.map(team => {
+                if (team.id === action.teamId) {
+                    return { ...team, players: [...team.players, action.player] };
+                } else {
+                    return team;
+                }
+            });
+        case "REMOVE_PLAYER":
+            return state.map(team => {
+                if (team.id === action.teamId) {
+                    return { ...team, players: team.players.filter(player => player.id !== action.playerId) };
+                } else {
+                    return team;
+                }
+            });
+        case "RENAME_TEAM":
+            return state.map(team => {
+                if (team.id === action.teamId) {
+                    return { ...team, name: action.name };
+                } else {
+                    return team;
+                }
+            });
+        case "RENAME_PLAYER":
+            return state.map(team => {
+                if (team.id === action.teamId) {
+                    return { ...team, players: team.players.map(player => player.id === action.playerId ? { ...player, name: action.name } : player) }
+                } else {
+                    return team;
+                }
+            });
+        case "ADD_POINT":
+            return state.map(team => {
+                if (team.id === action.teamId) {
+                    return { ...team, points: (team.points + action.points) }
+                } else {
+                    return team;
+                }
+            });
+        case "CHOOSE_TEAM":
+            return state.map(team => {
+                if (team.id === action.teamId) {
+                    return { ...team, current: true, players: team.players.map(player => player.id === action.playerId ? {...player, current: true} : {...player, current: false}) }
+                } else {
+                    return { ...team, current: false, players: team.players.map(player => player ? {...player, current:false} : player) };
+                }
+            });
+        default:
+            return state;
+    }
+}
