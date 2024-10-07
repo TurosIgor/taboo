49b06fd84b12dd3d03fd509e86356a534d1453a0
TurosIgor <valyp99@gmail.com>
2024-10-07 16:37:10 +0200

    basic page structure

--- taboo/src/pages/Results/Results.jsx
+++ taboo/src/pages/Results/Results.jsx
@@ -1,3 +1,19 @@
+import { useOutletContext } from "react-router-dom";
+import Scoreboard from "../../components/Scoreboard/Scoreboard";
+
 export default function Results () {
-    return <></>
+    const {teams, scores} = useOutletContext()
+
+    return (
+    <div className="Results">
+        <div className="Winner">
+            <h1 className="VictoryMessage"></h1>
+            <h2 className="MVP"></h2>
+        </div>
+        <div className="PlayerStats">
+
+        </div>
+        <Scoreboard teams={teams} scores={scores} />
+    </div>
+    )
 }
