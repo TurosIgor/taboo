45245d1b54630535b86891085523697ac60578b7
TurosIgor <valyp99@gmail.com>
2024-10-16 15:21:17 +0200

    adding basic messages to results

--- taboo/src/pages/Results/Results.jsx
+++ taboo/src/pages/Results/Results.jsx
@@ -1,18 +1,26 @@
 import { useOutletContext } from "react-router-dom";
 import Scoreboard from "../../components/Scoreboard/Scoreboard";
 
-export default function Results () {
-    const {teams} = useOutletContext()
-
-    function getWinner() {
-        return teams.find()
+function getWinner(array) {
+    let winner = array[0];
+    for(const item of array) {
+        if(item.points > winner.points) {
+            winner = item;
+        }
     }
+    return winner;
+}
+
+export default function Results () {
+    const {teams} = useOutletContext();
+    const winner = getWinner(teams);
+    const mvp = getWinner(winner.players);
 
     return (
     <div className="Results">
         <div className="Winner">
-            <h1 className="VictoryMessage"></h1>
-            <h2 className="MVP"></h2>
+            <h1 className="VictoryMessage">{winner.name} won with {winner.points} points!</h1>
+            <h2 className="MVP">{mvp.name} scored the most points for their team: {mvp.points}</h2>
         </div>
         <div className="PlayerStats">
 
