5808a702acdf4b4119acd6daf36d7fb6cc16a1a9
TurosIgor <valyp99@gmail.com>
2024-10-17 12:00:34 +0200

    WIP results page, some styling and animation

--- taboo/src/pages/Results/Results.jsx
+++ taboo/src/pages/Results/Results.jsx
@@ -1,5 +1,6 @@
 import { useOutletContext } from "react-router-dom";
 import Scoreboard from "../../components/Scoreboard/Scoreboard";
+import "./Results.css"
 
 function getWinner(array) {
     let winner = array[0];
@@ -19,8 +20,12 @@ export default function Results () {
     return (
     <div className="Results">
         <div className="Winner">
-            <h1 className="VictoryMessage">{winner.name} won with {winner.points} points!</h1>
-            <h2 className="MVP">{mvp.name} scored the most points for their team: {mvp.points}</h2>
+            <h1 className="VictoryMessage">Congratulations!</h1>
+            <h1 className="VictoryMessage"><span className={`WinnerName${winner.id.slice(5)}`}>{winner.name}</span> won with {winner.points} points!</h1>
+            <div className="Mvp">
+                <h1 className="MvpText"><span className="MvpM">M</span><span className="MvpV">V</span><span className="MvpP">P</span></h1>
+                <h2 className="MvpMessage"><span className={`MvpName${winner.id.slice(5)}`}>{mvp.name}</span> scored the most points for their team: {mvp.points}</h2>
+            </div>
         </div>
         <div className="PlayerStats">
 
