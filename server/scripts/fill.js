ec491f0603c63509f2b565f4c282dce69b6496fb
TurosIgor <valyp99@gmail.com>
2024-10-18 15:15:58 +0200

    modifying fill script to use new path to source data - local running

--- server/scripts/fill.js
+++ server/scripts/fill.js
@@ -7,13 +7,13 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const rootDir = path.resolve(__dirname, "../")
+const rootDir = path.resolve(__dirname, "../../")
 env.config({path: path.join(rootDir, '.env')})
 
 async function getDataFromJSON() {
     try{
-        const data = await fs.readFile(path.join(rootDir, 'taboo.json'), "utf8");
-        const { words } = await JSON.parse(data);
+        const data = await fs.readFile(path.join(rootDir, 'database/taboo.json'), "utf8");
+        const words = await JSON.parse(data);
         return words;
     } catch(err) {
         console.error(err);
